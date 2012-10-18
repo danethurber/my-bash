6443d762f119a82f58627e95d01482c92ce63ce9
Dane Thurber <dane.thurber@gmail.com>
2012-10-18 14:03:51 -0600

    display icon if repo is clean or dirty

--- main.sh
+++ main.sh
@@ -1,11 +1,7 @@
 source $MYBASH/git-completion.sh
 
 function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo -e "\e[0;31m⇪ \e[m " || echo -e "\e[0;32m✔ \e[m "
 }
 
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
-}
-
-export PS1='\n\e[0;37m\e[46m★ \e[m \e[1;36m\e[40m\W\e[m$(__git_ps1 ":\[\e[0;33m\]%s\[\e[0m\]\[\e[0;35m\]$(parse_git_dirty)\[\e[0m\]")$ '
+export PS1='$(__git_ps1 "$(parse_git_dirty)" )\e[1;36m\e[40m\W\e[m$(__git_ps1 ":\[\e[0;33m\]%s\[\e[0m\]")$ '
