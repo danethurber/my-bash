8bd8bd0e848c7a02e2d3cb4d9944ff4f9e0c3253
Dane Thurber <dane.thurber@gmail.com>
2012-10-31 21:28:43 -0600

    fix for upgrading to git 1.8

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ done
 unset file
 
 function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo -e "\e[0;31m⇪ \e[m " || echo -e "\e[0;32m✔ \e[m "
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo -e "\e[0;31m⇪ \e[m " || echo -e "\e[0;32m✔ \e[m "
 }
 
 export PS1='$(__git_ps1 "$(parse_git_dirty)" )\e[1;36m\e[40m\W\e[m$(__git_ps1 ":\[\e[0;33m\]%s\[\e[0m\]")$ '
